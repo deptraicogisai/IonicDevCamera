d50a783ab100813aa9944338e44dca5d4bb52ae1
Tran Phuoc Anh Vu <anhvu3890@gmail.com>
2018-01-31 16:33:52 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {File} from '@ionic-native/file';
+import {Transfer} from '@ionic-native/transfer';
+import {FilePath} from '@ionic-native/file-path';
+import {Camera} from '@ionic-native/camera';
+import {MediaCapture} from '@ionic-native/media-capture';
+import {Brightness} from "@ionic-native/brightness";
+import {Facebook} from "@ionic-native/facebook";
 
 @NgModule({
   declarations: [
@@ -24,7 +31,15 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    Facebook,
+    Brightness ,
+    File,
+    MediaCapture,
+    Transfer,
+    Camera,
+    FilePath,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,18 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {
+  ActionSheetController, Loading, LoadingController, NavController, Platform,
+  ToastController
+} from 'ionic-angular';
+
+import {File} from '@ionic-native/file';
+import {Transfer, TransferObject} from '@ionic-native/transfer';
+import {FilePath} from '@ionic-native/file-path';
+import {Camera, CameraOptions} from '@ionic-native/camera';
+import {MediaCapture, MediaFile, CaptureError, CaptureImageOptions} from '@ionic-native/media-capture';
+import {Brightness} from "@ionic-native/brightness";
+import {Facebook, FacebookLoginResponse} from "@ionic-native/facebook";
+
+declare var cordova: any;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +20,131 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  lastImage: string = null;
+  brightnessVal: number = 0.8;
+  userData: any;
 
+  constructor(private facebook: Facebook, private brightness: Brightness, public navCtrl: NavController, private camera: Camera, private transfer: Transfer, private file: File, private filePath: FilePath, public actionSheetCtrl: ActionSheetController, public toastCtrl: ToastController, public platform: Platform, public loadingCtrl: LoadingController
+    , public mediaCapture: MediaCapture) {
+    console.log('Faceook :' + this.facebook);
+  }
+
+  loginWithFB() {
+    this.facebook.login(['email', 'public_profile']).then((response: FacebookLoginResponse) => {
+      this.facebook.api('me?fields=id,name,email,first_name,picture.width(720).height(720).as(picture_large)', []).then(profile => {
+        this.userData = {email: profile['email'], first_name: profile['first_name'], picture: profile['picture_large']['data']['url'], username: profile['name']}
+      });
+    });
+  }
+
+  change() {
+    console.log('change');
+    this.brightness.setBrightness(this.brightnessVal / 100);
+  }
+
+  public takePicture(sourceType) {
+    // Create options for the Camera Dialog
+    var options: CameraOptions = {
+      quality: 100,
+      sourceType: sourceType,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    };
+
+    // Get the data of an image
+    this.camera.getPicture(options).then((imagePath) => {
+      // Special handling for Android library
+      if (this.platform.is('android') && sourceType === this.camera.PictureSourceType.PHOTOLIBRARY) {
+        this.filePath.resolveNativePath(imagePath)
+          .then(filePath => {
+            let correctPath = filePath.substr(0, filePath.lastIndexOf('/') + 1);
+            let currentName = imagePath.substring(imagePath.lastIndexOf('/') + 1, imagePath.lastIndexOf('?'));
+            this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+          });
+      } else {
+        console.log(imagePath);
+        this.lastImage = "data:image/jpeg;base64," + imagePath
+      }
+    }, (err) => {
+      this.presentToast('Error while selecting image.');
+    });
+  }
+
+// Create a new name for the image
+  private createFileName() {
+    var d = new Date(),
+      n = d.getTime(),
+      newFileName = n + ".jpg";
+    return newFileName;
+  }
+
+// Copy the image to a local folder
+  private copyFileToLocalDir(namePath, currentName, newFileName) {
+    this.file.copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(success => {
+      this.file.readAsDataURL(cordova.file.dataDirectory, newFileName).then(result => {
+        this.lastImage = newFileName;
+      });
+    }, error => {
+      this.presentToast('Error while storing file.');
+    });
+  }
+
+  private presentToast(text) {
+    let toast = this.toastCtrl.create({
+      message: text,
+      duration: 3000,
+      position: 'top'
+    });
+    toast.present();
+  }
+
+// Always get the accurate path to your apps folder
+  public pathForImage(img) {
+    if (img === null) {
+      return '';
+    } else {
+      var filePath = cordova.file.dataDirectory + img;
+      this.file.readAsDataURL(cordova.file.dataDirectory, img).then(result => {
+
+      });
+    }
+  }
+
+  public presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Select Image Source',
+      buttons: [
+        {
+          text: 'Load from Library',
+          handler: () => {
+            this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+          }
+        },
+        {
+          text: 'Use Camera',
+          handler: () => {
+            this.takePicture(this.camera.PictureSourceType.CAMERA);
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  recordVideo() {
+    let options: CaptureImageOptions = {limit: 3};
+    this.mediaCapture.captureImage()
+      .then(
+        (data: MediaFile[]) => console.log(data),
+        (err: CaptureError) => console.error(err)
+      );
   }
 
 }
